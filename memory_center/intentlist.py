6c1e00e2e3d829d49f9b3ca3555274ce6f3c0c0d
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-21 14:01:30 +0800

    Update intentlist.py

--- memory_center/intentlist.py
+++ memory_center/intentlist.py
@@ -2,9 +2,47 @@
 
 import sys
 import os
+import setting
+import time
 
 def intentlistturn():
     url = ''
+    data = {
+            'ClientEvent': {
+                'Header': {
+                    'NameSpace': 'xiaolan.client.intentlist.get',
+                    'TimeStamp': int(time.time()),
+                    'RequestsId': time.time(),
+                    'RequestsFrom': setting.setting()['main_setting']['clienttype'],
+                    'ClientId': setting.setting()['main_setting']['clientid']
+                },
+                'ConversationInfo': {
+                    'ConversationId': None,
+                    'ShouldHandlerSkill': None,
+                    'SkillShouldHandler': None,
+                    'SkillAwakenKeyword': None,
+                    'SendToSkillInfo': {
+                        'Intent': None,
+                        'Text': None,
+                        'Slots': None
+                    }
+                }
+            },
+            'Debug': {
+                'TimeStamp': str(int(time.time())),
+                'ClientId': setting.setting()['main_setting']['clientid'],
+                'States': {
+                    'ClientStates': ['serviceing'],
+                    'NluStates': ['waitIntentList'],
+                    'SttStates': ['emptying'],
+                    'TtsStates': ['emptyling']
+                },
+                'Commands': {
+                    'ClientCommands': ['service for user'],
+                    'elseCommands': []
+                }
+            }
+        }
     r = requests(url,
                  data=data)
     return r.json()
