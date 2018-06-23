18544c9f14f8c30d929cdf999494274bdfdd335f
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-23 21:22:34 +0800

    Update display.py

--- display_center/display.py
+++ display_center/display.py
@@ -9,11 +9,12 @@ sys.path.append('/home/pi/xiaolan/')
 from speech_center.stt import baidu_stt
 from speech_center.tts import baidu_tts
 from speech_center.tts import youdao_tts
+from speech_center.stt import ifly_stt
 import speech_center.speaker
 
-def dis(json, intentdict, tok):
-    bt = baidu_tts()
-    bs = baidu_stt(1, 2, 3, 4)
-    if json['card'] == None or json['cord'] == '':
-        bt.tts(json['outputSpeech'], tok)
-        speaker.speak()
+class ScreenDisplay(object):
+    
+    def __init__(self):
+        pass
+    def CardDo(self, info):
+        pass
