069ba5da6588d239feecdc5959680058426d1e04
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-23 18:47:48 +0800

    Create commandlist.py

---
+++ memory_center/commandlist.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import demjson
+import base64
+import hashlib
+sys.path.append('/home/pi/xiaolan')
+from network_center.xiaolanClientToServer import 
