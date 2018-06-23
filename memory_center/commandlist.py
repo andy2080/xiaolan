ce8edebecd30c81335e8155b5445b4239a30071f
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-23 19:08:12 +0800

    Update commandlist.py

--- memory_center/commandlist.py
+++ memory_center/commandlist.py
@@ -7,4 +7,19 @@ import demjson
 import base64
 import hashlib
 sys.path.append('/home/pi/xiaolan')
-from network_center.xiaolanClientToServer import 
+from network_center.xiaolanClientToServer import xClientToServer
+from network_center.DuerosClientToServer import dClientToServer
+
+class CommandsList(object):
+   
+    def __init__(self):
+        self.xc = xClientToServer
+        self.dc = dClientToServer
+    def GetCommandsList(self, types):
+        if types == 'xiaolan':
+            lists = self.xc.SendReqToServer(data)
+            return lists
+        elif types == 'dueros':
+            lists = self.dc.SendReqToServer(url, data)
+          
+    
