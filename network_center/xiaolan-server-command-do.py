98233881a665d41900af6b9dc1d591d19f024117
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-23 18:37:07 +0800

    Update xiaolan-server-command-do.py

--- network_center/xiaolan-server-command-do.py
+++ network_center/xiaolan-server-command-do.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import os
+import json
+import base64
+import hashlib
 import requests
 import data
-from 
+sys.path.append('/home/pi/xiaolan/')
+from memory_center.commandslist import commandslist
+from speech_center.conversation import dialogue
+
+
