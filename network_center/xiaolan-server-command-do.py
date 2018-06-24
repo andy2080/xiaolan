a65e9fce29cf1464a3905dcab5279a5e83f44d08
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-24 14:44:02 +0800

    Update xiaolan-server-command-do.py

--- network_center/xiaolan-server-command-do.py
+++ network_center/xiaolan-server-command-do.py
@@ -6,14 +6,62 @@ import json
 import base64
 import hashlib
 import requests
-import data
+import time
+import xiaolanClientToServer
 sys.path.append('/home/pi/xiaolan/')
-from memory_center.commandslist import commandslist
+from memory_center.commandlist import clist
 from speech_center.conversation import dialogue
 
 class CommandsDo(object):
   
     def _init__(self):
-        pass
+        self.xcts = xClientToServer()
     def Do(self, respones):
-        pass
+        syscommands = respones['ClientShouldDo']['System']['commands']
+        sysmore = respones['ClientShouldDo']['System']['more']
+        if syscommands == 'ShutDown':
+            os.system('sudo poweroff')
+        elif syscommands == 'Reboot':
+            os.system('sudo reboot')
+        elif syscommands == 'SendLog':
+            data = {
+                        'ClientEvent': {
+                            'Header': {
+                                'NameSpace': 'ClientLog',
+                                'TimeStamp': int(time.time),
+                                'RequestsId': '8476',
+                                'RequestsFrom': setting.setting()['main_setting']['ClientType'],
+                                'ClientId': setting.setting()['main_setting']['ClientId']
+                            },
+                            'ConversationInfo': {
+                                'ConversationId': None,
+                                'ShouldHandlerSkill': None,
+                                'SkillShouldHandler': None,
+                                'SkillAwakenKeyword': None,
+                                'SendToSkillInfo': {
+                                      'Intent': None,
+                                      'Text': None,
+                                      'Slots': {
+                                          'slot1': None
+                                      }
+                                }
+                            }
+                        },
+                        'Debug': {
+                            'TimeStamp': str(time.time()),
+                            'ClientId': setting.setting()['main_setting']['ClientId'],
+                            'States': {
+                                'ClientStates': ['working'],
+                                'NluStates': ['emptying'],
+                                'SttStates': ['emptying'],
+                                'TtsStates': ['emptying']
+                            },
+                            'Commands': {
+                                'ClientCommands': [],
+                                'elseCommands': []
+                            }
+                        }
+            }
+            self.xcts.DiyReq(data)
+        elif syscommands == '
+        
