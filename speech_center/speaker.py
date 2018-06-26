e95fcd25d9a867ad800721271cf706d9eb0c1aa9
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-26 13:31:20 +0800

    Update speaker.py

--- speech_center/speaker.py
+++ speech_center/speaker.py
@@ -5,24 +5,24 @@ import os
 
 def ding(): #开始录制指令提示音
 
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/ding.wav')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/ding.wav')
 
 def dong(): #结束录制指令提示音
 
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/dong.wav')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/dong.wav')
 
 def kacha(): #拍照声
     
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/kacha.mp3')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/kacha.mp3')
     
 def speak(): #说出的回话
     
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/say.mp3')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/say.mp3')
 
 def play(song_name): #音乐播放器
     
     print('正在播放:' + song_name)
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/music.mp3')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/music.mp3')
 
 def stop():
 
@@ -32,4 +32,12 @@ def stop():
         os.system('python /home/pi/auditory_center/awaken/snowboy.py')
 def speacilrecorder():
 
-    os.system('omxplayer /home/pi/xiaolan/memory_center/musiclib/speacilrecorder.mp3')
+    os.system('mplayer /home/pi/xiaolan/memory_center/musiclib/speacilrecorder.mp3')
+    
+def DiyOmxSpeaker(outputdevice, fileroad):
+    
+    os.system('omxplayer -r -o ' + outputdevice +  ' ' + fileroad)
+    
+def DiyMplayerSpeaker(fileroad):
+    
+    os.system('mplayer ' + fileroad)
