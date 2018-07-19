74304eb5fa81fb6d628711da33d3566ed73db508
xiaoland <1481605673@qq.com>
2018-07-19 17:40:56 +0800

    update

--- Base.py
+++ Base.py
@@ -4,35 +4,34 @@ import os
 import sys
 import json
 import requests
-import setting
-from memory_center.Log import Log
-from memory_center.DateBase import DateBase
-from speech_center.stt import baidu_stt
-from speech_center.stt import ifly_stt
-from speech_center.tts import baidu_tts
-from speech_center.tts import youdao_tts
-from speech_center.conversation import dialogue
-from speech_center.nlu import Nlu
-from network_center.xiaolanClientToServer import ClientToServer
-from network_center.xiaolanServerCommandDo import CommandsDo
-from auditory_center.recorder import recorder
-from auditory_center.awaken.snowboy import snowboy
-
 
 
 class xiaolanBase(object):
 
     def __init__(self):
 
+        import setting
         self.set = setting.setting()
+        from memory_center.Log import Log
         self.Log = Log()
+        from memory_center.DateBase import DateBase
         self.Datebase = DateBase()
+        from speech_center.conversation import dialogue
         self.dialogue = dialogue()
+        from auditory_center.recorder import recorder
         self.recorder = recorder()
+        from auditory_center.awaken.snowboy import snowboy
         self.snowboy = snowboy()
+        from speech_center.nlu import Nlu
         self.Nlu = Nlu()
+        from network_center.xiaolanClientToServer import ClientToServer
         self.ClientToServer = ClientToServer()
+        from network_center.xiaolanServerCommandDo import CommandsDo
         self.CommandsDo = CommandsDo()
+        from speech_center.stt import baidu_stt
+        from speech_center.stt import ifly_stt
+        from speech_center.tts import baidu_tts
+        from speech_center.tts import youdao_tts
         if self.set['main_setting']['TTS']['service'] == 'baidu':
             self.tts = baidu_tts()
             self.tok = self.tts.get_token()
