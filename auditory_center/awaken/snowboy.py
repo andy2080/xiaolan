97ba768709059ed759e00a0aff5d37fe38ce2ea8
xiaoland <1481605673@qq.com>
2018-08-04 10:46:17 +0800

    update
    Bug fix

--- auditory_center/awaken/snowboy.py
+++ auditory_center/awaken/snowboy.py
@@ -18,7 +18,7 @@ class Snowboy(xiaolanBase):
         detector = snowboydecoder.HotwordDetector("/home/pi/xiaolan/auditory_center/awaken/hotword/jarvis.umdl,/home/pi/xiaolan/auditory_center/awaken/hotword/xiaodu_l12r10_sen_35_35_32_highsen_40_40_39_0104_kuanyang.umdl", sensitivity=[0.8, 0.8, 0.4, 0.4, 0.39], audio_gain=1, apply_frontend=True)
         detector.start(a)
 
-def a(self):
+def a():
     try:
         sys.exit(-1)
     except SystemExit:
